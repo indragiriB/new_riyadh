13fb3759906861977d762c2558378dd8f32114fa
indragiriB <indragiribhakti@gmail.com>
2026-03-13 13:22:14 +0700

    add ayat

--- resources/views/quran/show.blade.php
+++ resources/views/quran/show.blade.php
@@ -1,4 +1,23 @@
-<div class="container">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Amiri&display=swap" rel="stylesheet">
+
+<style>
+    .teks-arab {
+        font-family: 'Amiri', serif;
+        direction: rtl; 
+        font-size: 32px;
+        line-height: 2.5;
+    }
+</style>
+</head>
+<body>
+    <div class="container">
     <h1>{{ $surat['namaLatin'] }} ({{ $surat['nama'] }})</h1>
     <p><em>{{ $surat['arti'] }} - {{ $surat['jumlahAyat'] }} Ayat</em></p>
     <hr>
@@ -9,20 +28,21 @@
         </h3>
     @endif
 
-    @foreach($surat['ayat'] as $ayat)
-        <div class="ayat-item" style="margin-bottom: 40px; text-align: right;">
-            <h2 style="line-height: 2.5;">
-                {{ $ayat['teksArab'] }} 
-                <span style="border: 1px solid #000; border-radius: 50%; padding: 5px 10px; font-size: 14px;">
-                    {{ $ayat['nomorAyat'] }}
-                </span>
-            </h2>
-            
-            <div style="text-align: left; margin-top: 10px;">
-                <p><em>{{ $ayat['teksLatin'] }}</em></p>
-                <p>{{ $ayat['teksIndonesia'] }}</p>
-            </div>
-        </div>
-        <hr>
-    @endforeach
+@foreach($surat['ayat'] as $ayat)
+    <div style="margin-bottom: 50px;">
+        <h2 class="teks-arab" style="text-align: right;">
+            {{ $ayat['teksArab'] }} 
+            <span style="font-size: 18px; border: 1px solid #ccc; border-radius: 50%; padding: 5px;">
+                {{ $ayat['nomorAyat'] }}
+            </span>
+        </h2>
+
+        <p style="color: green;"><em>{!! $ayat['teksLatin'] !!}</em></p>
+        
+        <p>{{ $ayat['teksIndonesia'] }}</p>
+    </div>
+    <hr>
+@endforeach
 </div>
+</body>
+</html>
