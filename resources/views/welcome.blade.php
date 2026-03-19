66dfd6e06fe15026eb4e8d11c60cb88834212300
indragiriB <indragiribhakti@gmail.com>
2026-03-19 23:45:07 +0700

    body or whatever

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        body{
+            padding: 0;
+            margin: 0;
+            }
+        .first{
+            background-color: red;
+            width: 100vw;
+            height: 100vh;
+        }
+    </style>
 </head>
 <body>
+    <div class="first">
+
+    </div>
     <a href="{{route('quran.index')}}">menuju quran</a>
 </body>
 </html>
