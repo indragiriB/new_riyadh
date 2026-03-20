94ee48a05b4cfc2cbd9083d12aefa296b3a54bce
indragiriB <indragiribhakti@gmail.com>
2026-03-20 21:11:06 +0700

    add react

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,27 @@
 <?php
-use App\Http\Controllers\QuranController;
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/quran', [QuranController::class, 'index'])->name('quran.index');
-Route::get('/quran/{nomor}', [QuranController::class, 'show'])->name('quran.show');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
